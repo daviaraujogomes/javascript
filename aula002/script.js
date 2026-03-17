f53956a0dbf6950ec448b5efbb5ef1dada93ce59
Davi Araujo <daviaraujogomes2008@gmail.com>
2026-03-17 15:57:38 -0300

    Lista de tarefas com javascript

---
+++ aula002/script.js
@@ -0,0 +1,10 @@
+
+    function adicionarTarefa() {
+      //let mensagem = "Tarefa adicionada com sucesso!";
+            
+        let inputTarefa = document.getElementById("inputTarefa")
+        let tarefa = inputTarefa.value
+        document.getElementById("mensagem").textContent = tarefa;
+
+        inputTarefa.value = ""
+}
